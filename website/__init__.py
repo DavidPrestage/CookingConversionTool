e6c9a05b59f582c3dd6b04144023a6d2fb3a5d09
Achro777 <david.prestage@hotmail.com>
2022-06-05 16:29:13 +0100

    PostConversionToPostgres

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,23 @@
 import flask
-from flaskext.mysql import MySQL
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = flask.Flask(__name__)
-mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = '6xCQXFD8'
-app.config['MYSQL_DATABASE_DB'] = 'cooking'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
 
-conn = mysql.connect()
-cursor = conn.cursor()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cooking'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+session = db.session()
+
+
+#mysql = MySQL()
+
+
+#app.config['MYSQL_DATABASE_PASSWORD'] = '6xCQXFD8'
+#app.config['MYSQL_DATABASE_DB'] = 'cooking'
+#app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+#mysql.init_app(app)
+
+#conn = mysql.connect()
+#cursor = conn.cursor()
