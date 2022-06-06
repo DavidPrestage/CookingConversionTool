1ccda5e486f1527c9a8639f035308232b1de73b5
Achro777 <david.prestage@hotmail.com>
2022-06-06 12:29:23 +0100

    heroku commit

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 # Entry point for app.
 from app import app    # Allows app to be discovered by 'flask' command.
 from app import views  # Connects view.py to website
+
+if __name__ == "__main__":
+    app.run()
