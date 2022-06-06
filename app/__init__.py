8603e9650786ba810ba9d32ce9f6f074922401e3
Achro777 <david.prestage@hotmail.com>
2022-06-06 12:46:53 +0100

    heroku commit

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cooking'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://skjelkjuitnsni:ddab36c82a87dd55a3612302fdee487e0eea5f11dd11449e322053edf74de0c5@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/d4f1evpqdmj7ks'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://skjelkjuitnsni:ddab36c82a87dd55a3612302fdee487e0eea5f11dd11449e322053edf74de0c5@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/d4f1evpqdmj7ks'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
